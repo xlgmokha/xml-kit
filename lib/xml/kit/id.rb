972ff284c0669380f0c1923b6f6b88e2f5d6b5f9
mo <mo.khan@gmail.com>
2017-12-30 13:55:43 -0700

    add space.

--- lib/xml/kit/id.rb
+++ lib/xml/kit/id.rb
@@ -1,7 +1,7 @@
 module Xml
   module Kit
     # This class is used primary for generating ID.
-    #https://www.w3.org/2001/XMLSchema.xsd
+    # https://www.w3.org/2001/XMLSchema.xsd
     class Id
       # Generate an ID that conforms to the XML Schema.
       # https://www.w3.org/2001/XMLSchema.xsd
